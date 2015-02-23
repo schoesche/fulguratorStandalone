90119553969c890c1e42f18032596874303c44b7
schoesche <ga@schoesche.ch>
2015-02-23 13:38:13 +0100

    formated

--- app/app.js
+++ app/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
 angular.module('linksprivate', [
     'ngRoute',
     'smart-table'
@@ -11,13 +10,7 @@ angular.module('linksprivate', [
     }]).
 
     controller('LinksCtrl', ['$scope', '$filter', '$http', function (scope, filter, http) {
-   /*     scope.rowCollection = [
-            {name: 'RedNet', tag: 'rednet mobiliar', link: 'https://rednet.mobi.mobicorp.ch/ssp_ds/'},
-            {name: 'RedNet1', tag: 'rednet1 mobiliar', link: 'https://rednet.mobi.mobicorp.ch/ssp_ds/'},
-            {name: 'RedNet2', tag: 'rednet2 mobiliar', link: 'https://rednet.mobi.mobicorp.ch/ssp_ds/'},
-            {name: 'RedNet3', tag: 'rednet3 mobiliar', link: 'https://rednet.mobi.mobicorp.ch/ssp_ds/'}
-        ];
-*/
+
         var loadLinkFile = function() {
             http.get('links.json')
                 .then(function(res){
@@ -30,7 +23,6 @@ console.log(scope.rowCollection);
         loadLinkFile();
 
 
-        /*scope.predicates = ['firstName', 'lastName', 'birthDate', 'balance', 'email'];*/
         scope.predicates = ['name', 'tag', 'link'];
         scope.selectedPredicate = scope.predicates[0];
 
