1d50d9f120802eb5792ce968881cc99e64ec7164
schoesche <ga@schoesche.ch>
2015-02-25 08:21:00 +0100

    add new view

--- app/app.js
+++ app/app.js
@@ -7,22 +7,37 @@ angular.module('fulgurator', [
 ]).
 
     config(['$routeProvider', function ($routeProvider) {
-
+        $routeProvider.
+            when('/',
+            {
+                templateUrl: 'components/linklist/linklist.html',
+                controller: 'LinksCtrl'
+            }).
+            when('/settings',
+            {
+                templateUrl: 'components/settings/settings.html',
+                controller: 'LinksSettingsCtrl'
+            }).
+           /*when('/page-3',
+            {
+                templateUrl: 'partials/partial-For-routes-3.html',
+                controller: appCtrl
+            }).*/
+            otherwise({redirectTo: ("/")})
     }]).
 
     controller('LinksCtrl', ['$scope', '$filter', '$http', 'linksModule', function (scope, filter, http, linksModule) {
 
-        var loadLinkFile = function() {
+        var loadLinkFile = function () {
             /*
-            http.get('links.json')
-                .then(function(res){
-                        scope.rowCollection = res.data;
-            });
-            */
+             http.get('links.json')
+             .then(function(res){
+             scope.rowCollection = res.data;
+             });
+             */
 
 
-            scope.rowCollection =  linksModule.allLinks();
-
+            scope.rowCollection = linksModule.allLinks();
 
 
         }
@@ -33,6 +48,10 @@ angular.module('fulgurator', [
         scope.predicates = ['name', 'tag', 'link'];
         scope.selectedPredicate = scope.predicates[0];
 
+
+    }]).
+
+    controller('LinksSettingsCtrl', ['$scope', '$filter', '$http', 'linksModule', function (scope, filter, http, linksModule) {
 
     }]);
 
