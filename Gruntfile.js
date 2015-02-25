67e29b37fe5b88fa3ea1c9dc967d416e9cfa8a57
schoesche <ga@schoesche.ch>
2015-02-25 13:20:23 +0100

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,76 @@
 module.exports = function(grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
-            }
-        }
+    // load build config
+    var options = require('./build.config');
+
+    // load task configurations
+    var configs = require('load-grunt-configs')(grunt, options);
+
+    // initialize loaded configs
+    grunt.initConfig(configs);
+
+    // load tasks dynamically
+    require('jit-grunt')(grunt, {
+        htmlbuild : 'grunt-html-build',
+        ngconstant : 'grunt-ng-constant',
+        includereplace : 'grunt-include-replace'
     });
 
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    require('time-grunt')(grunt);
 
-    // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    /**
+     * Register tasks
+     */
+
+    grunt.registerTask('default', 'the default task', [
+        'run:dev:local'
+    ]);
+
+    grunt.registerTask('run:dev:local', 'run the application on a local server and watching files', [
+        'clean', 'build:dev:local', 'connect:server', 'concurrent:watchingDevLocal'
+    ]);
+
+    grunt.registerTask('watching:dev', 'watch source file for changes (for rappit develop)', [
+        'build:dev', 'concurrent:watchingDev'
+    ]);
+
+    grunt.registerTask('build:dev:local', 'build for local development (without backend)', [
+        'clean',
+        'jshint',
+        /*'less:dev',*/
+        'html2js:dev',
+        'ngconstant:devLocal',
+        'copy:common',
+        'sync:dev',
+        'includereplace:devLocal',
+        'htmlbuild'
+    ]);
+
+    grunt.registerTask('build:dev', 'build for local development', [
+        'clean',
+        'jshint',
+        /*'less:dev',*/
+        'html2js:dev',
+        'ngconstant:dist',
+        'copy:common',
+        'sync:dev',
+        'includereplace:dist',
+        'htmlbuild'
+    ]);
+
+    grunt.registerTask('build:dist', 'build the full distribution', [
+        'clean',
+        'jshint',
+        /*'less:dist',*/
+        'html2js:dist',
+        'ngconstant:dist',
+        'concat',
+        'ngAnnotate',
+        'uglify',
+        'copy:common',
+        'copy:dist',
+        'includereplace:dist',
+        'htmlbuild'
+    ]);
 
 };
--- app/data/links.js
+++ app/data/links.js
@@ -13,14 +13,14 @@ angular.module('fulgurator.linksModule', [])
                     "groupname": "Allgemein",
                     "grouplinks": [
                         {
-                            "name": "RedNet",
-                            "tag": "rednet mobiliar rn",
-                            "link": "https://rednet.mobi.mobicorp.ch/ssp_ds/home.html"
+                            "name": "google",
+                            "tag": "google",
+                            "link": "https://www.google.ch"
                         },
                         {
-                            "name": "Wiki ELAN",
-                            "tag": "elan wiki wikielan elanwiki",
-                            "link": "https://mobiwiki.mobicorp.ch/wiki/index.php?title=Hauptseite_ElanWiki"
+                            "name": "google",
+                            "tag": "google",
+                            "link": "https://www.google.ch"
                         }
                     ]
                 },
@@ -28,134 +28,14 @@ angular.module('fulgurator.linksModule', [])
                     "groupname": "Development",
                     "grouplinks": [
                         {
-                            "name": "AMW",
-                            "tag": "amw umgebungen",
-                            "link": "https://unix-infra.mobi.ch/AMW_web"
+                            "name": "google",
+                            "tag": "google",
+                            "link": "https://www.google.ch"
                         },
                         {
-                            "name": "Jenkins Baseline",
-                            "tag": "jenkins baseline build server",
-                            "link": "https://baseline.mobicorp.test:8443/"
-                        },
-                        {
-                            "name": "Jenkins CI",
-                            "tag": "jenkins ci build server",
-                            "link": "https://ci.mobicorp.test:8443/"
-                        },
-                        {
-                            "name": "Architektur Guidelines",
-                            "tag": "guidelines architektur",
-                            "link": "http://guidelines.mobicorp.test:8080/dashboard.action"
-                        },
-                        {
-                            "name": "SOAP Repo",
-                            "tag": "soap repo webservice",
-                            "link": "http://soarepo.mobicorp.test:8080/soarepo/pages/index.xhtml"
-                        },
-                        {
-                            "name": "Wiki Elan Testumgebungen",
-                            "tag": "wiki elan testumbegungen",
-                            "link": "https://mobiwiki.mobicorp.ch/wiki/index.php/Noch_mehr_zu_Testumgebungen"
-                        },
-                        {
-                            "name": "Masterliste Applikation",
-                            "tag": "masterliste Applikation",
-                            "link": "https://rednet.mobi.mobicorp.ch/mobidoc/Rednet/de/07_Unternehmenseinheiten/05_IT/nonindex_IT_misc/masterliste_de.xlsx"
-                        }
-                    ]
-                },
-                {
-                    "groupname": "System Dokumentation",
-                    "grouplinks": [
-                        {
-                            "name": "Wiki ELAN neue Mitarbeiter",
-                            "tag": "Wiki elan neue mitarbeiter",
-                            "link": "https://mobiwiki.mobicorp.ch/wiki/index.php/Willkommen_im_ELAN_Team"
-                        },
-                        {
-                            "name": "Wiki ELAN neue Mitarbeiter",
-                            "tag": "Wiki elan neue mitarbeiter",
-                            "link": "https://mobiwiki.mobicorp.ch/wiki/index.php/Willkommen_im_ELAN_Team"
-                        },
-                        {
-                            "name": "Glossar",
-                            "tag": "Mobiliar glossar",
-                            "link": "https://rednet.mobi.mobicorp.ch/ssp_ds/arbeitsmittel-glossare.html"
-                        }
-                    ]
-                },
-                {
-                    "groupname": "Tools",
-                    "grouplinks": [
-                        {
-                            "name": "Lifecycle Management",
-                            "tag": "quality center lifecycle management applikation application",
-                            "link": "  https://pzone2.mobi.ch/qcbin/start_a.jsp"
-                        },
-                        {
-                            "name": "BMC",
-                            "tag": "bmc tickets 3rdLevel",
-                            "link": "https://itsm.mobi.mobicorp.ch/arsys/forms/itsm/SHR:LandingConsole/Default+Administrator+View/?cacheid=1537590a"
-                        },
-                        {
-                            "name": "Subversion",
-                            "tag": "svn repo",
-                            "link": "https://svn.mobicorp.ch/svn/"
-                        }
-                    ]
-                },
-                {
-                    "groupname": "Betrieb",
-                    "grouplinks": [
-                        {
-                            "name": "BMC",
-                            "tag": "bmc tickets 3rdLevel",
-                            "link": "https://itsm.mobi.mobicorp.ch/arsys/forms/itsm/SHR:LandingConsole/Default+Administrator+View/?cacheid=1537590a"
-                        },
-                        {
-                            "name": "Wiki Elan Support",
-                            "tag": "wiki elan Support 3rdLevel ",
-                            "link": "https://mobiwiki.mobicorp.ch/wiki/index.php/Know-how_Elan_Support"
-                        },
-                        {
-                            "name": "Datenkreislauf Einstiegsseite",
-                            "tag": "Datenkreislauf Einstiegsseite Daten Transfer Host Extraktor DB2",
-                            "link": "http://s90214/"
-                        },
-                        {
-                            "name": "Wiki bekannte Probleme",
-                            "tag": "bekannte Probleme 3rd Level support kategorie archiv",
-                            "link": "https://mobiwiki.mobicorp.ch/wiki/index.php/Kategorie:Elan_Problem:Archiv"
-                        },
-                        {
-                            "name": "Wiki wichtige kontakte",
-                            "tag": "wichtige kontake elan support know-how",
-                            "link": "https://mobiwiki.mobicorp.ch/wiki/index.php/Wichtige_Kontakte"
-                        },
-                        {
-                            "name": "Wiki kopieren von Partnerdaten",
-                            "tag": "elan wiki kopieren partnerdaten host",
-                            "link": "https://mobiwiki.mobicorp.ch/wiki/index.php/Host-Funktionen"
-                        },
-                        {
-                            "name": "F&A Board",
-                            "tag": "f und a fa f&a board",
-                            "link": "http://mobidoc.mobi.ch/cgi-bin2/FAB2/faboard.pl?Name=ELANFABoard&sprache_inhalt=de"
-                        },
-                        {
-                            "name": "VEOTRAC1 Table",
-                            "tag": "code schlüssel schluessel key partner vertrag initiating machine type of change c40000",
-                            "link": "https://mobiwiki.mobicorp.ch/wiki/index.php/VEOTRAC1"
-                        },
-                        {
-                            "name": "Wiki Informationen zu Tabellen",
-                            "tag": "table db elan information c999 hf VEO VTO",
-                            "link": "https://mobiwiki.mobicorp.ch/wiki/index.php/Informationen_zu_Tabellen"
-                        },
-                        {
-                            "name": "Wiki Logs besser verstehen",
-                            "tag": "alle elan log files walkbacks session logs user logs server logs fehler normalbetrieb",
-                            "link": "https://mobiwiki.mobicorp.ch/wiki/index.php/Alle_Elan_Log_Files"
+                            "name": "google",
+                            "tag": "google",
+                            "link": "https://www.google.ch"
                         }
                     ]
                 }
